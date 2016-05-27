5fddebcae997db9e1a4108413d73b0d057a61e83
songchengjiang <moses_jc@sina.com>
2016-05-27 15:57:30 +0800

    parameter corrected

--- cocos/platform/CCGLView.cpp
+++ cocos/platform/CCGLView.cpp
@@ -481,7 +481,7 @@ void GLView::renderScene(Scene* scene, Renderer* renderer)
     }
     else
     {
-        scene->render(renderer, nullptr, nullptr);
+        scene->render(renderer, Mat4::IDENTITY, nullptr);
     }
 }
 
